bb40d88095449191dcbf7fe67c8cb44a98a05c63
Jessie Xu <jessie.xu@oocl.com>
2019-03-18 20:04:06 +0800

    use eureka

--- src/main/java/com/ItemApplication.java
+++ src/main/java/com/ItemApplication.java
@@ -2,7 +2,12 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * 声明这是Eureka的client端
+ */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ItemApplication {
     public static void main(String[] args) {
